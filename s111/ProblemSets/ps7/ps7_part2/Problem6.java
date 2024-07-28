276c119c7dbe79d74462515474705781c7ff23ad
Kqpa <76552592+Kqpa@users.noreply.github.com>
2024-07-28 13:57:49 -0400

    okay

--- s111/ProblemSets/ps7/ps7_part2/Problem6.java
+++ s111/ProblemSets/ps7/ps7_part2/Problem6.java
@@ -9,7 +9,7 @@ public class Problem6 {
         int j = 0;
         for (int i = 0; i < arr.length - 1; i++)
         {
-            if (arr[i] != arr[i + 1])
+            if (arr[i] == arr[i + 1])
             {
                 arr[i] = Integer.MIN_VALUE;
                 zeroCount++;
@@ -31,7 +31,7 @@ public class Problem6 {
     }
 
     public static void main(String[] args) {
-        int[] a1 = {-1, -1, -2, 0, 0, 2, 2, 5, 5, 10, 12, 12 , 13 ,13 ,13};
+        int[] a1 = {0, 2, 5, 12, 13, 13, 0, 0, 0, 0, 0, 0};
         int numUnique = removeDups(a1);
         System.out.println(Arrays.toString(a1));
         System.out.println(numUnique);
